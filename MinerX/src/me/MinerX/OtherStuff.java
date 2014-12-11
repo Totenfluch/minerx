e9b7f013c6ac5e26dd7993207acd5a76915883b3
ChekaZ <o.zieglerZ@web.de>
2014-12-11 20:56:25 +0100

    Difficulty API updated

--- MinerX/src/me/MinerX/OtherStuff.java
+++ MinerX/src/me/MinerX/OtherStuff.java
@@ -16,8 +16,8 @@ public class OtherStuff {
 	public static String pFTCPriceinUSD = null;
 	public static String pFTCDiff = null;
 	public static boolean pFTCPriceinUSDisPulled = false;
-	
-	
+
+
 	public static void getFTCPriceUSD() throws IOException{
 
 		final String userAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.8.1.12) Gecko/20080201 Firefox/2.0.0.12";
@@ -34,7 +34,7 @@ public class OtherStuff {
 			} 
 			in.close(); 
 			pFTCPriceinUSD ="1 FTC = "+ str + "$";
-			
+
 		} catch (MalformedURLException e) 
 		{
 		} 
@@ -51,18 +51,22 @@ public class OtherStuff {
 
 		final String userAgent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.8.1.12) Gecko/20080201 Firefox/2.0.0.12";
 		try {
-			URL url = new URL("http://api.feathercoin.com/?output=difficulty&json=0");
+			URL url = new URL("http://api.feathercoin.com/?output=stats");
 			URLConnection conn = url.openConnection();
 			conn.addRequestProperty("User-Agent", userAgent);
 
 			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String str, str1; 
-			str = new String();
-			while ((str1 = in.readLine()) != null) {
-				str = str + str1;
-			} 
+			String split = in.readLine();
+			String difficulty = " ";
+			String[] parts = split.split(",");
+			for(int i=0;i<parts.length;i++){
+				if(parts[i].contains("nowdiff")){
+					difficulty = parts[i];
+					difficulty = difficulty.substring(10, 17);
+				}
+			}
 			in.close(); 
-			pFTCDiff ="Difficulty = "+ str;
+			pFTCDiff ="Difficulty = "+ difficulty;
 		} catch (MalformedURLException e) 
 		{
 		} 
@@ -70,63 +74,63 @@ public class OtherStuff {
 		{
 		} 
 	}
-	
+
 	public static void poolInfoFTCMuddy() throws IOException, URISyntaxException {
-		  if(java.awt.Desktop.isDesktopSupported() ) {
-		        java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+		if(java.awt.Desktop.isDesktopSupported() ) {
+			java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
 
-		        if(desktop.isSupported(java.awt.Desktop.Action.BROWSE) ) {
-		          java.net.URI uri = new java.net.URI("http://us.mine-ftc.co.uk:19327/static/");
-		              desktop.browse(uri);
-		        }
-		  } 
+			if(desktop.isSupported(java.awt.Desktop.Action.BROWSE) ) {
+				java.net.URI uri = new java.net.URI("http://us.mine-ftc.co.uk:19327/static/");
+				desktop.browse(uri);
+			}
+		} 
 	}
-	
+
 	public static void poolInfoFTCWellen() throws IOException, URISyntaxException {
-		  if(java.awt.Desktop.isDesktopSupported() ) {
-		        java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+		if(java.awt.Desktop.isDesktopSupported() ) {
+			java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
 
-		        if(desktop.isSupported(java.awt.Desktop.Action.BROWSE) ) {
-		          java.net.URI uri = new java.net.URI("http://p2pool.neoscrypt.de:19327");
-		              desktop.browse(uri);
-		        }
-		  } 
+			if(desktop.isSupported(java.awt.Desktop.Action.BROWSE) ) {
+				java.net.URI uri = new java.net.URI("http://p2pool.neoscrypt.de:19327");
+				desktop.browse(uri);
+			}
+		} 
 	}
-	
+
 	public static void poolInfoFTCLizhi() throws IOException, URISyntaxException {
-		  if(java.awt.Desktop.isDesktopSupported() ) {
-		        java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+		if(java.awt.Desktop.isDesktopSupported() ) {
+			java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
 
-		        if(desktop.isSupported(java.awt.Desktop.Action.BROWSE) ) {
-		          java.net.URI uri = new java.net.URI("http://pool.ftc-c.com:19327/static/");
-		              desktop.browse(uri);
-		        }
-		  } 
+			if(desktop.isSupported(java.awt.Desktop.Action.BROWSE) ) {
+				java.net.URI uri = new java.net.URI("http://pool.ftc-c.com:19327/static/");
+				desktop.browse(uri);
+			}
+		} 
 	}
-	
+
 	public static void CoinCalcFTC() throws IOException, URISyntaxException {
-		  if(java.awt.Desktop.isDesktopSupported() ) {
-		        java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+		if(java.awt.Desktop.isDesktopSupported() ) {
+			java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
 
-		        if(desktop.isSupported(java.awt.Desktop.Action.BROWSE) ) {
-		          java.net.URI uri = new java.net.URI("http://Feathercoin.com/calc/");
-		              desktop.browse(uri);
-		        }
-		  } 
+			if(desktop.isSupported(java.awt.Desktop.Action.BROWSE) ) {
+				java.net.URI uri = new java.net.URI("http://Feathercoin.com/calc/");
+				desktop.browse(uri);
+			}
+		} 
 	}
-	
+
 	public static boolean checkURL(String website) throws IOException {
-		
+
 		try{
-		    final URLConnection connection = new URL(website).openConnection();
-		    connection.connect();
-		    return true;
+			final URLConnection connection = new URL(website).openConnection();
+			connection.connect();
+			return true;
 		} catch(final IOException e){
 			return false;
 		}
-		
+
 	}
-	
+
 }
 
 
